eb388dedb92b9cd6552762fc130e59c50d012594
Muhammad Farooq <fm6182218@gmail.com>
2025-08-05 00:21:11 +0500

    done

---
+++ script.js
@@ -0,0 +1,49 @@
+
+gsap.from(".item", {
+    x: 20,
+    opacity: 0,
+    duration: 0.8,
+    delay: 0.5,
+});
+gsap.from(".name", {
+    x: 20,
+    opacity: 0,
+    duration: 0.8,
+    delay: 0.5,
+});
+gsap.from(".img", {
+    duration: 1,
+    opacity: 0,
+    y: 20,
+    delay: 0.5,
+});
+gsap.from(".btn", {
+    duration: 1,
+    opacity: 0,
+    y: 20,
+    delay: 0.5,
+});
+
+// const btn = document.querySelector(".btn-1");
+// const from = document.querySelector(".form");
+// const main = document.querySelector(".main");
+
+// btn.addEventListener("click", () => {
+
+//     main.style.display = "none";
+//     from.style.display = "block";
+// })
+const btn = document.querySelector(".btn-1");
+const form = document.querySelector(".form");
+const main = document.querySelector(".main");
+
+btn.addEventListener("click", () => {
+    form.style.display = "block";
+    main.style.display = "none"
+    gsap.from(".form", {
+        duration: 1,
+        opacity: 0,
+        y: 30,
+    });
+});
+
